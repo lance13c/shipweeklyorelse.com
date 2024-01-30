2ac3d36c2a8ee5f348aa8a5c06063c71abb16e54
Dominic Cicilio <dominic@yoamigo.io>
2024-01-30 00:50:02 -0500

    updates the footer

--- components/client-pages/home.tsx
+++ components/client-pages/home.tsx
@@ -102,9 +102,6 @@ const HomePage = () => {
           </div>
         </motion.div>
       </main>
-      <footer className="pt-20 md:pt-0 px-[8vw] text-white bg-gray-900 flex-1 flex items-end relative bottom-0 w-full p-2 text-xs">
-        <span className="bottom-0">© 2024 shipweeklyorelse.com brought to you by YoAmigo LLC all rights reserved</span>
-      </footer>
     </>
   );
 };
